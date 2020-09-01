d7883784c6545a7edb21c85667164e55cb3635eb
uni-8 <julia.zhol.a@gmail.com>
2020-09-01 15:31:07 +0300

    Updates

---
+++ precache-manifest.7eb754987c991c5868a3c93317fd7dc3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6bdd6acce8a0b5d015f39fd1ab5e7e5",
+    "url": "./index.html"
+  },
+  {
+    "revision": "52353b2d67f84b723297",
+    "url": "./static/css/main.964b1fed.chunk.css"
+  },
+  {
+    "revision": "36689209a43d29ff1e8c",
+    "url": "./static/js/2.d4c7f416.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.d4c7f416.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "52353b2d67f84b723297",
+    "url": "./static/js/main.ff2666ea.chunk.js"
+  },
+  {
+    "revision": "ec438ca3dae444460a4d",
+    "url": "./static/js/runtime-main.1d48480d.js"
+  }
+]);
